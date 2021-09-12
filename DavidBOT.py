cc8e4463426e82ba1053ea4c8e9916143d90cc5a
David Kundih <kundihdavid@gmail.com>
2021-09-12 13:48:23 +0200

    Update DavidBOT.py

--- DavidBOT.py
+++ DavidBOT.py
@@ -5,7 +5,7 @@ from discord.ext import commands, tasks
 from itertools import cycle
 
 client = commands.Bot(command_prefix='.')
-status = cycle(['pip install alunari', 'github.com/dkundih', 'pypi.org/user/dkundih'])
+status = cycle(['pip install alunari', 'pip install alunariTools', 'github.com/dkundih', 'pypi.org/user/dkundih'])
 
 @client.event
 async def on_ready():
@@ -42,13 +42,6 @@ async def _skare(ctx):
     elif odg == 'Papir.':
       await ctx.send(f'{odg}\n\n=> Ovaj put pobjeđuješ. <=')
 
-@client.command(aliases=['pergar', 'Pergar'])
-async def _pergar(ctx):
-    responses = [
-        'Boli me patka, realno.', 'Report vam ne gine.', 'Ne, ne ja nečem.', 'Vreeedu pajdo.', 'Pratiš ve igru?', 'Deca se igrala pa se zaigrala.', 'Ma more mi saftati pitona.', 'Ak sam seenal ne znači ka sam videl.', 'Dečki znate kaj je sirnica?', 'Deste bratići?', 'To ti je od mobitela.', 'Pa vi ste samo hejteri.' 
-    ]
-    await ctx.send(f'Matija Pergar BOT:  {random.choice(responses)}')
-
 @client.command(aliases=['Papir', 'papir', 'Papir.', 'papir.'])
 async def _papir(ctx):
     responses = [
@@ -67,4 +60,4 @@ async def _alunari(ctx):
     await ctx.send('GITHUB: https://github.com/dkundih/alunari\nPYPI: https://pypi.org/user/dkundih/')
 
 keep_alive()
-client.run('ODg0NTI0MjY4MjU0Mjk4MTUz.YTZvaw.MDkU748tNgRB7RD6TpQ41ekiEBw')
+client.run('CENSORED') #Instead of 'CENSORED' paste 'Token-Code'.
